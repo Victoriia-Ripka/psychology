7b134e5ef363e784d11e34418e3e27f0292544f2
Victoriia-Ripka <vikaripka@gmail.com>
2023-04-30 11:16:26 +0200

    reviews start

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,12 +1,92 @@
 import React from "react";
 import { Container } from "../styles.styled";
-import { ReviewsContainer, Title } from "./Reviews.styled";
+import {
+  ReviewsContainer,
+  Title,
+  SlideContainer,
+  Slide,
+  User,
+  Info,
+  Img,
+  UserInfo,
+  Name,
+  Country,
+  Review,
+} from "./Reviews.styled";
 
 const Reviews = () => {
   return (
     <Container>
       <ReviewsContainer>
         <Title>Відгуки моїх клієнтів</Title>
+        <div>
+          <SlideContainer>
+            <Slide>
+              <User>
+                <Info>
+                  <Img src="" alt="" />
+                  <UserInfo>
+                    <Name>Олександра</Name>
+                    <Country>США</Country>
+                  </UserInfo>
+                </Info>
+                <Review>
+                  Мій шлях до терапії був довгим. Чи то я пручалася, чи не була
+                  готова - досі не знаю, але знаю точно, що дуже рада, що
+                  знайшла Світлану. Роки чотири тому моя найкраща подруга
+                  працювала зі Світланою. І я бачила результат цієї праці.
+                  Плідний та дуже стрімкий. І в момент, коли я все ж таки
+                  наважилася знайти собі власного терапевта, я звернулася саме
+                  до неї. Я не знала чи ми поладнаємо і нам буде комфортно...
+                </Review>
+              </User>
+            </Slide>
+          </SlideContainer>
+          <SlideContainer>
+            <Slide>
+              <User>
+                <Info>
+                  <Img src="" alt="" />
+                  <UserInfo>
+                    <Name>Олександра</Name>
+                    <Country>США</Country>
+                  </UserInfo>
+                </Info>
+                <Review>
+                  Мій шлях до терапії був довгим. Чи то я пручалася, чи не була
+                  готова - досі не знаю, але знаю точно, що дуже рада, що
+                  знайшла Світлану. Роки чотири тому моя найкраща подруга
+                  працювала зі Світланою. І я бачила результат цієї праці.
+                  Плідний та дуже стрімкий. І в момент, коли я все ж таки
+                  наважилася знайти собі власного терапевта, я звернулася саме
+                  до неї. Я не знала чи ми поладнаємо і нам буде комфортно...
+                </Review>
+              </User>
+            </Slide>
+          </SlideContainer>
+          <SlideContainer>
+            <Slide>
+              <User>
+                <Info>
+                  <Img src="" alt="" />
+                  <UserInfo>
+                    <Name>Олександра</Name>
+                    <Country>США</Country>
+                  </UserInfo>
+                </Info>
+                <Review>
+                  Мій шлях до терапії був довгим. Чи то я пручалася, чи не була
+                  готова - досі не знаю, але знаю точно, що дуже рада, що
+                  знайшла Світлану. Роки чотири тому моя найкраща подруга
+                  працювала зі Світланою. І я бачила результат цієї праці.
+                  Плідний та дуже стрімкий. І в момент, коли я все ж таки
+                  наважилася знайти собі власного терапевта, я звернулася саме
+                  до неї. Я не знала чи ми поладнаємо і нам буде комфортно...
+                </Review>
+              </User>
+            </Slide>
+          </SlideContainer>
+        </div>
       </ReviewsContainer>
     </Container>
   );
--- src/components/Reviews/Reviews.styled.js
+++ src/components/Reviews/Reviews.styled.js
@@ -26,7 +26,6 @@ export const Title = styled.h2`
 
   @media ${device.mobileTablet} {
     text-align: left;
-
   }
   @media ${device.tablet} {
     font-size: 20px;
@@ -36,4 +35,67 @@ export const Title = styled.h2`
     font-size: 32px;
     margin-bottom: 60px;
   }
+`;
+
+export const SlideContainer = styled.div``;
+
+export const Slide = styled.div`
+  background: #ffffff;
+  border: 1px solid #594d46;
+  border-radius: 25px;
+  padding: 25px;
+  width: 265px;
+`;
+
+export const User = styled.div`
+  // display: flex;
+  // align-items: center;
+`;
+
+export const Info = styled.div`
+  display: flex;
+`;
+
+export const Img = styled.img`
+  width: 27px;
+  height: 27px;
+  border-radius: 50%;
+  background: #d9d9d9;
+  object-fit: cover;
+`;
+
+export const UserInfo = styled.div`
+  margin-left: 12px;
+  margin-bottom: 5px;
+`;
+
+export const Name = styled.p`
+  font-family: "Lora";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+
+  color: #001a23;
+  margin-bottom: 2px;
+`;
+
+export const Country = styled.p`
+  font-family: "Lora";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+
+  color: #000000;
+`;
+
+export const Review = styled.p`
+  font-family: "Lora";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+
+  color: #000000;
 `;
